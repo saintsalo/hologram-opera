f3d766cff26f9e12fdb042e56902988048e43eeb
DL Salo <salo@objectsonore.com>
2025-01-29 10:24:09 -0800

    add newsletter load issue

--- components/Newsletter.tsx
+++ components/Newsletter.tsx
@@ -1,28 +1,33 @@
 "use client"
-import React, { useEffect, useState } from "react"
+import React, { useEffect } from "react"
+
+declare global {
+  interface Window {
+    hbspt: any
+  }
+}
 
 const Newsletter: React.FC = () => {
-  const [isClient, setIsClient] = useState(false)
-
   useEffect(() => {
-    setIsClient(true)
-  }, [])
+    const script = document.createElement("script")
+    script.src = "https://js.hsforms.net/forms/v2.js"
+    document.body.appendChild(script)
 
-  if (!isClient) {
-    return null
-  }
+    script.addEventListener("load", () => {
+      if (window.hbspt) {
+        window.hbspt.forms.create({
+          portalId: "49007331",
+          formId: "609b80d3-a044-4283-a65b-999ce144a3fa",
+          target: "#hubspotForm",
+        })
+      }
+    })
+  }, [])
 
   return (
     <div className="w-[800px] p-8 flex flex-col gap-8 items-center justify-center ">
       <h4>Signup for our Newsletter!</h4>
-      <div
-        className="hs-form-frame w-full"
-        data-region="na1"
-        data-form-id="609b80d3-a044-4283-a65b-999ce144a3fa"
-        data-portal-id="49007331"
-      ></div>
-
-      <script src="https://js.hsforms.net/forms/embed/49007331.js" defer></script>
+      <div className="w-full" id="hubspotForm"></div>
     </div>
   )
 }
